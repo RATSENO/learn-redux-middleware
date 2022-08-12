0584b0c43c0a0c7d893608b556b1b234edab1434
RATSENO <onestar1014@gmail.com>
2022-08-12 11:01:09 +0900

    비동기 카운터 만들기

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import { createLogger } from "redux-logger";
 import App from "./App";
 import rootReducer from './modules';
 import ReduxThunk from 'redux-thunk';
+import createSagaMiddleware from "@redux-saga/core";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const logger = createLogger();
+const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer : rootReducer,
-  middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger).concat(ReduxThunk)
+  middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger).concat(ReduxThunk).concat(sagaMiddleware),
+  devTools : composeWithDevTools()
 });
 
 const container = document.getElementById("root");
